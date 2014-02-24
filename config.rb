96a30897755522ec25388e175595c6d07ec3f182
Louis Rose <louismrose@gmail.com>
2014-02-24 19:03:42 +0000

    Moved essays to their own folder.

--- config.rb
+++ config.rb
@@ -45,11 +45,10 @@
 #   end
 # end
 
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+set :layouts_dir, 'assets/layouts'
 
 set :relative_links, true
 
