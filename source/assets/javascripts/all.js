e5b27e9ef91cb37225d534fa5863906506c24d48
Louis Rose <louismrose@gmail.com>
2014-02-24 19:17:04 +0000

    Bootstrapify.

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,3 +1,9 @@
 //= require_tree .
 //= require jquery-2.0.3.min
 //= require bootstrap
+
+$(function() {
+  var pageId = document.body.className;
+  var navItem = $("#navigation li#" + pageId)[0];
+  navItem.className = "active";
+});
